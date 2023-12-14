c2781b68ac05bb43972953160a9aa9b43de3cf89
samuel <shemlouk@gmail.com>
2023-12-14 11:02:13 -0300

    fix: add cors to allow any origin

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from "@elysiajs/cors";
 import { html } from "@elysiajs/html";
 import staticPlugin from "@elysiajs/static";
 import { Elysia } from "elysia";
@@ -6,6 +7,7 @@ import { pagesRoute } from "./routes/pages";
 import { tasksRoute } from "./routes/tasks";
 
 const app = new Elysia()
+  .use(cors())
   .use(staticPlugin())
   .use(html())
   .use(tasksRoute)
